5adf18fa1094d1134c412ba643f8509cbec64f7c
Julian Macioce <jjmacioce@hotmail.com>
2023-01-16 15:50:06 -0500

    Adding App User

---
+++ Enums/Provinces.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Connectify.Enums
+{
+    public class Provinces
+    {
+        AB,
+        BC,
+        MB,
+        NB,
+        NL,
+        NS,
+        NT,
+        NU,
+        ON,
+        PE,
+        QC,
+        SK,
+        YT
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Connectify.Models
+{
+    public class Category
+    {
+        public int ID { get; set; }
+
+        [Required]
+        public string? AppUserID { get; set; }
+
+        [Required]
+        [Display(Name ="Category Name")]
+        public string? Name { get; set;}
+
+        //Virtuals
+        public virtual AppUser? AppUser { get; set; }
+        public virtual ICollection<Contact> Contacts { get; set; } = new HashSet<Contact>();
+    }
+}
