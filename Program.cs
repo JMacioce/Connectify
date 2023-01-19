05c576de93cfadc080c3e1648e300460fcab6e53
Julian Macioce <jjmacioce@hotmail.com>
2023-01-18 21:56:13 -0500

    add environment variables for heroku hosting, and write a helper to keep our database in sync

--- Program.cs
+++ Program.cs
@@ -5,12 +5,14 @@ using Microsoft.EntityFrameworkCore;
 using Connectify.Services;
 using Connectify.Services.Interfaces;
 using Microsoft.AspNetCore.Identity.UI.Services;
+using Connectify.Helpers;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 //var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-var connectionString = builder.Configuration.GetSection("pgSettings")["pgConnection"];
+//var connectionString = builder.Configuration.GetSection("pgSettings")["pgConnection"];
+var connectionString = ConnectionHelper.GetConnectionString(builder.Configuration);
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(connectionString));
